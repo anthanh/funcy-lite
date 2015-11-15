d78bc0777a59147300ec0d16dae35e5c730f6c72
Anthanh Pham Trinh <antai.pt@gmail.com>
2015-11-15 15:25:16 +0100

    gruntfile formatted

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
 
-  // Define the configuration for all the tasks
-  grunt.initConfig({
+    // Define the configuration for all the tasks
+    grunt.initConfig({
 
-    // The actual grunt server settings
-    connect: {
+        // The actual grunt server settings
+        connect: {
 
-      options: {
-        port: 9000,
-        livereload: 35729,
-        keepalive: true,
-        // Change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
+            options: {
+                port: 9000,
+                livereload: 35729,
+                keepalive: true,
+                // Change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
 
-      livereload: {
-        options: {
-          base: ['app']
+            livereload: {
+                options: {
+                    base: ['app']
+                }
+            }
+
         }
-      }
 
-    }
+    });
 
-  });
-
-  grunt.registerTask('serve', 'start the server and preview your app', ['connect:livereload']);
+    grunt.registerTask('serve', 'start the server and preview your app', ['connect:livereload']);
 
 };
