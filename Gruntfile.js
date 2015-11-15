22e94775f4239a46bc69f4105b3f4e16cbfd0256
Anthanh Pham Trinh <antai.pt@gmail.com>
2015-11-15 16:46:23 +0100

    Grunt exercice solution

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,71 @@ module.exports = function(grunt) {
     // Define the configuration for all the tasks
     grunt.initConfig({
 
+        clean: ['.tmp', 'dist'],
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+            },
+            all: ['app/scripts/**/*.js']
+        },
+
+        watch: {
+
+            js: {
+                files: ['app/scripts/**/*.js'],
+                tasks: ['jshint']
+            },
+
+            livereload: {
+                options: {
+                    livereload: 35729
+                },
+                files: [
+                    'app/*.html',
+                    'app/**/*.js',
+                    'app/**/*.css'
+                ]
+            }
+        },
+
+        useminPrepare: {
+            options: {
+                dest: 'dist',
+            },
+            html: ['app/*.html']
+        },
+
+        usemin: {
+            html: ['dist/**/*.html']
+        },
+
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'app',
+                    src: [
+                        '**/*',
+                        '!vendor/**/*',
+                        '!scripts/**/*'
+                    ],
+                    dest: 'dist/'
+                }]
+            },
+            bootstrap: {
+                files: [{
+                    expand: true,
+                    // cwd: 'bower',
+                    src: [
+                        'bower_components/bootstrap/dist/css/bootstrap.css',
+                        'bower_components/bootstrap/dist/fonts/*',
+                    ],
+                    dest: 'dist/'
+                }]
+            }
+        },
+
         // The actual grunt server settings
         connect: {
 
@@ -18,13 +83,21 @@ module.exports = function(grunt) {
                 port: 9000,
                 livereload: 35729,
                 keepalive: true,
+                open: true,
                 // Change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
 
             livereload: {
                 options: {
+                    keepalive: false,
                     base: ['app']
+                }
+            },
+
+            dist: {
+                options: {
+                    base: ['dist']
                 }
             }
 
@@ -32,6 +105,21 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.registerTask('serve', 'start the server and preview your app', ['connect:livereload']);
+    grunt.registerTask('serve', 'start the server and preview your app', ['connect:livereload', 'watch']);
+
+    grunt.registerTask('serve:dist', 'start the server and preview your app', ['build', 'connect:dist']);
+
+    grunt.registerTask('build', [
+        'clean',
+        'jshint',
+        'useminPrepare',
+        'concat:generated',
+        'uglify',
+        'copy:main',
+        'copy:bootstrap',
+        'usemin'
+    ]);
+
+    grunt.registerTask('default', ['build']);
 
 };
